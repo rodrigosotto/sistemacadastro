dd984b7419f02fc7493f633b4b3524564dc4170e
Jeffersontads <jeffersontads@hotmail.com>
2020-11-04 17:10:29 -0300

    remover dados de conexao

--- conexao_db.php
+++ conexao_db.php
@@ -2,25 +2,23 @@
 
 /*conexão com o banco de dados*/
 $servername = "127.0.0.1";
-$username = "jefferson";
-$password = "30302220";
+$username = "User_name";
+$password = "senha";
 
 $conn = new PDO("mysql:host=$servername;dbname=restaurante_db", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// CRIANDO A CONEXÃO (EXISTE MUITOS EXEMPLOS W3SCHOOL ETC ETC)
 try {
     $conn = new PDO("mysql:host=$servername;dbname=restaurante_db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //    echo "Conexão realizada com sucesso";
 
-    //verifica se email e senha estão nao banco de dados (prepara uma consulta um SQL)
+    //verifica se email e senha estão nao banco de dados e prepara a consulta
     $consulta = $conn->prepare("SELECT codigo FROM usuario WHERE email=:email AND senha=md5(:senha)");
     $consulta->bindParam(':email', $email, PDO::PARAM_STR);//vai ser substituida pela variavel $email
     $consulta->bindParam(':senha', $senha, PDO::PARAM_STR);//vai ser substituida pela variavel $senha
     $consulta->execute();
 
-    // set the resulting array to associative
     $result = $consulta->fetchAll();
     $qtd_user = count($result); //adiciono os user em uma variavel
 //        var_dump($qtd_user);
@@ -37,5 +35,5 @@ try {
     echo "Conexão falhou: " . $e->getMessage();
 }
 
-//fecha conexao com o banco de dados ainda mais quando o sistema é utilizado por muitas pessoas, isso evita de deixar a conexao com o banco de dados em aberto
+//fecha conexao com o DB
 $conn = null;

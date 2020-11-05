fc65e359c7b73b1cc040423fdb7ef55ecdf11835
Jeffersontads <jeffersontads@hotmail.com>
2020-11-05 16:56:44 -0300

    Ajustar novo arquivo de conexao com o banco de dados

--- conexao_db.php
+++ conexao_db.php
@@ -2,8 +2,8 @@
 
 /*conexão com o banco de dados*/
 $servername = "127.0.0.1";
-$username = "jefferson";
-$password = "30302220";
+$username = "nome";
+$password = "senha";
 
 $conn = new PDO("mysql:host=$servername;dbname=restaurante_db", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

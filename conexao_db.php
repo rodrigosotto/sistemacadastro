5aff06feb6c88998b15423723e99319748362ff2
Jeffersontads <jeffersontads@hotmail.com>
2020-11-10 11:27:27 -0300

    Ajustar commit anterior

--- conexao_db.php
+++ conexao_db.php
@@ -2,8 +2,8 @@
 
 /*conexão com o banco de dados*/
 $servername = "localhost";
-$username = "teste";
-$password = "teste3";
+$username = "usuario";
+$password = "senha";
 
 $conn = new PDO("mysql:host=$servername;dbname=restaurante_db", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
